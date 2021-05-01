bf2b8e1b7b3b70f29c538a41f0f931dfdbe1c971
owen-duncan-snobel <oadster@cse.yorku.ca>
2021-05-01 10:47:47 -0400

    mongoose added for connection to db for backend

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const port = 5000;
 const cors = require('cors');
 const jwt = require('express-jwt');
 const jwks = require('jwks-rsa');
 const axios = require('axios');
 
-const app = express();
-app.use(cors());
+require('dotenv').config();
 
-const jwtCheck = jwt({
-	secret: jwks.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: 'https://outplacement.us.auth0.com/.well-known/jwks.json',
-	}),
-	audience: 'outplacementexpress',
-	issuer: 'https://outplacement.us.auth0.com/',
-	algorithms: ['RS256'],
-}).unless({ path: ['/'] });
+mongoose
+	.connect(
+		'mongodb+srv://admin:' +
+			process.env.MONGODB_PASSWORD +
+			'@cluster0.4yrwu.mongodb.net/db-name?retryWrites=true&w=majority',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => {
+		const app = express();
+		app.use(cors());
 
-app.use(jwtCheck);
+		const jwtCheck = jwt({
+			secret: jwks.expressJwtSecret({
+				cache: true,
+				rateLimit: true,
+				jwksRequestsPerMinute: 5,
+				jwksUri:
+					'https://outplacement.us.auth0.com/.well-known/jwks.json',
+			}),
+			audience: 'outplacementexpress',
+			issuer: 'https://outplacement.us.auth0.com/',
+			algorithms: ['RS256'],
+		}).unless({ path: ['/'] });
 
-app.get('/', (req, res) => {
-	res.send('unprotected');
-});
+		app.use(jwtCheck);
 
-app.get('/protected', (req, res) => {
-	res.send({ hello: 'Hello World!' });
-});
+		app.get('/', (req, res) => {
+			res.send('unprotected');
+		});
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
+		app.get('/protected', (req, res) => {
+			res.send({ hello: 'Hello World!' });
+		});
+
+		app.listen(port, () => {
+			console.log(`Example app listening at http://localhost:${port}`);
+		});
+	});
